622ef2e7ddbbc1cf8394f5645f94c2f78e8e9af4
Ricciard0150 <richard.melo@alunos.sc.senac.br>
2026-03-17 09:38:20 -0300

    Scripts d dialogo

---
+++ Assets/Scritps/Texting.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+
+public class DialogueManager : MonoBehaviour
+{
+    [Header("Di�logo deste NPC")]
+    [TextArea(3, 6)]
+    public string[] falas;
+
+    [Header("Configura��o de Intera��o")]
+    public KeyCode teclaInteracao = KeyCode.E;
+
+    private bool playerInRange = false;
+    private bool dialogueStarted = false;
+
+    private void Update()
+    {
+        if (playerInRange && !dialogueStarted && Input.GetKeyDown(teclaInteracao))
+        {
+            IniciarDialogo();
+        }
+    }
+
+    private void IniciarDialogo()
+    {
+        if (TextManager.Instance != null)
+        {
+            dialogueStarted = true;
+            TextManager.Instance.StartDialogue(falas);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.TryGetComponent(out IStatusPlayer statuys))
+            return;
+        playerInRange = true;
+
+    }
+   
+}
