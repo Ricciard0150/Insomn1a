5c8a8779cc6364ae7cd8ab2cd8d9428e4e436fed
Ricciard0150 <richard.melo@alunos.sc.senac.br>
2026-04-01 13:55:58 -0300

    timeline transition

--- Assets/Scritps/PlayerTeleport.cs
+++ Assets/Scritps/PlayerTeleport.cs
@@ -1,28 +1,42 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using System.Collections;
 
 public class PlayerTeleport : MonoBehaviour
 {
-    [Header("booleans")]
-
-    bool isColliding = false;
-
-    [Header("UI")]
-
     public Transform target;
     public Transform teleportUI;
+    public PlayableDirector director;
 
-    void Update()
+    public float delay = 0.5f; 
+
+    bool isTeleporting = false;
+
+    private void Start()
     {
-        if (isColliding)
-        {
-            target.position = teleportUI.position;
-        }
+        director.Stop();
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.TryGetComponent(out IStatusPlayer status))
+        if (collision.TryGetComponent(out IStatusPlayer status))
         {
-            isColliding = true;
+            if (!isTeleporting)
+            {
+                StartCoroutine(TeleportWithDelay());
+            }
         }
+    }
+
+    IEnumerator TeleportWithDelay()
+    {
+        isTeleporting = true;
+
+        yield return new WaitForSeconds(delay);
+
+        target.position = teleportUI.position;
+        director.Play();
+
+        isTeleporting = false;
     }
 }
