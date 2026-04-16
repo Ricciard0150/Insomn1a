3af8a98b7e41d5063d6603ded2f9bf3c24a26c56
Ricciard0150 <richard.melo@alunos.sc.senac.br>
2026-04-16 16:25:24 -0300

    bugfix no teleport do player

---
+++ Assets/RPGscripts/DIalogueRPGs.cs
@@ -0,0 +1,116 @@
+﻿using TMPro;
+using UnityEngine;
+using System.Collections;
+
+public class DIalogueRPGs : MonoBehaviour
+{
+    [Header("UI")]
+    public GameObject dialoguePanel;
+    public TMP_Text dialogueText;
+
+    [Header("Config")]
+    public string[] falas;
+    public float velocidadeTexto = 0.05f;
+    public KeyCode teclaInteragir = KeyCode.E;
+    public KeyCode teclaFechar = KeyCode.Q;
+
+    private int index = 0;
+    private bool terminouFrase = false;
+    private bool dialogoAtivo = false;
+    private bool jaAtivado = false; 
+
+    private Coroutine typingCoroutine;
+
+    public TopDownMovement tdm;
+
+    void Update()
+    {
+        if (!dialogoAtivo) return;
+
+        if (Input.GetKeyDown(teclaFechar))
+        {
+            EncerrarDialogo();
+            return;
+        }
+
+        if (Input.GetKeyDown(teclaInteragir))
+        {
+            if (!terminouFrase)
+            {
+                if (typingCoroutine != null)
+                    StopCoroutine(typingCoroutine);
+
+                dialogueText.text = falas[index];
+                terminouFrase = true;
+            }
+            else
+            {
+                index++;
+
+                if (index < falas.Length)
+                {
+                    typingCoroutine = StartCoroutine(EscreverTexto());
+                }
+                else
+                {
+                    EncerrarDialogo();
+                    Debug.Log("aaa");
+
+
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (jaAtivado) return; 
+
+        if (collision.TryGetComponent(out IStatusPlayer status))
+        {
+            jaAtivado = true;
+            IniciarDialogo();
+        }
+    }
+
+    void IniciarDialogo()
+    {
+        tdm.canMove = false;
+        dialogoAtivo = true;
+        dialoguePanel.SetActive(true);
+
+        index = 0;
+
+        if (typingCoroutine != null)
+            StopCoroutine(typingCoroutine);
+
+        typingCoroutine = StartCoroutine(EscreverTexto());
+    }
+
+    void EncerrarDialogo()
+    {
+        if (typingCoroutine != null)
+            StopCoroutine(typingCoroutine);
+
+        dialoguePanel.SetActive(false);
+        dialogoAtivo = false;
+
+        Destroy(gameObject);
+        tdm.canMove = true;
+
+    }
+
+    IEnumerator EscreverTexto()
+    {
+        terminouFrase = false;
+        dialogueText.text = "";
+
+        foreach (char letra in falas[index])
+        {
+            dialogueText.text += letra;
+            yield return new WaitForSeconds(velocidadeTexto);
+        }
+
+        terminouFrase = true;
+    }
+}
