34d94404433b7f4cd2a6d9cd3fb80494b6f47c0d
Ricciard0150 <richard.melo@alunos.sc.senac.br>
2026-08-12 16:23:11 -0300

    Start back

--- Assets/Scritps/Dialogue/StartDialogue.cs
+++ Assets/Scritps/Dialogue/StartDialogue.cs
@@ -5,26 +5,12 @@ public class AutoDialogueStarter : MonoBehaviour
     [SerializeField] private DialogueSystem dialogueSystem;
     [SerializeField] private NPCData npcData;
     [SerializeField] private float delayToStart = 0.5f;
-    [SerializeField] private string dialogueID = "UniqueDialogue"; // Identificador único para este diálogo em todo o jogo
 
     private bool started = false;
 
     void Start()
     {
-        // Verifica se este diálogo específico já foi executado em TODO O JOGO
-        string playerPrefKey = $"GlobalDialogue_{dialogueID}";
-
-        if (PlayerPrefs.GetInt(playerPrefKey, 0) == 0)
-        {
-            // Primeira vez que este diálogo é executado em toda a vida do jogo
-            Invoke(nameof(StartDialogue), delayToStart);
-        }
-        else
-        {
-            // Já foi executado antes em alguma cena anterior
-            Debug.Log($"Diálogo '{dialogueID}' já foi executado anteriormente em todo o jogo.");
-            Destroy(this); // Ou gameObject.SetActive(false);
-        }
+        Invoke(nameof(StartDialogue), delayToStart);
     }
 
     void Update()
@@ -45,10 +31,5 @@ public class AutoDialogueStarter : MonoBehaviour
         dialogueSystem.SetNPCDialogue(npcData.falas, npcData.sprites);
         dialogueSystem.StartDialogue();
         started = true;
-
-        // Marca que este diálogo já foi executado em todo o jogo
-        string playerPrefKey = $"GlobalDialogue_{dialogueID}";
-        PlayerPrefs.SetInt(playerPrefKey, 1);
-        PlayerPrefs.Save();
     }
 }
